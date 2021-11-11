e698726556a6e6683dc25aad5177d7221ec66c03
Jeremy <tommyhilfiger12@hotmail.com>
2021-11-10 20:03:44 -0600

    centered login/signup/ added them to hamburger menu

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,39 +1,34 @@
-import { Button, Col, Container, Form, Popover, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form } from 'react-bootstrap';
 import './Login.css';
 
 const Login = () => {
-  return (
-    <div>
-      <Popover id="popover-basic">
-        <Popover.Header as="h3">Login</Popover.Header>
-        <Popover.Body>
-          <Container className="formCont">
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail" >
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" required />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+	return (
+		<div className='form'>
+			<Container className="formCont">
+				<Form>
+					<Form.Group className="mb-3" controlId="formBasicEmail">
+						<Form.Label>Email Adress:</Form.Label>
+						<Form.Control type="email" placeholder="Enter Email" required />
+						<Form.Text className="text-muted">
+							We'll never share your email with anyone else.
+						</Form.Text>
+					</Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" required />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Forget me not!" />
-              </Form.Group>
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Container>
-        </Popover.Body>
-      </Popover>
-    </div>
-  );
-  // Trigger code must go HERE
+					<Form.Group className="mb-3" controlId="formBasicPassword">
+						<Form.Label>Password:</Form.Label>
+						<Form.Control type="password" placeholder="Password" required />
+					</Form.Group>
+					<Form.Group className="mb-3" controlId="formBasicCheckbox">
+						<Form.Check type="checkbox" label="Forget me not!" />
+					</Form.Group>
+					<Button variant="primary" type="submit">
+						Submit
+					</Button>
+				</Form>
+			</Container>
+		</div>
+	);
+	// Trigger code must go HERE
 };
 
 export default Login;
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -3,99 +3,86 @@ import './Signup.css';
 import Country from './Country';
 
 function Signup() {
-  return (
-    <div>
-      <Popover id="popover-basic">
-        <Popover.Header as="h3">Signup</Popover.Header>
-        <Popover.Body>
-          <Container className="formCont">
-            <Form>
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridFirstNamel">
-                  <Form.Label>First Name</Form.Label>
-                  <Form.Control type="text" placeholder="First Namel" />
-                </Form.Group>
+	return (
+		<div className="signupForm">
+			<Container className="formCont">
+				<Form>
+					<Row className="mb-3">
+						<Form.Group as={Col} controlId="formGridFirstNamel">
+							<Form.Label>First Name:</Form.Label>
+							<Form.Control type="text" placeholder="First Name" />
+						</Form.Group>
 
-                <Form.Group as={Col} controlId="formGridLastName">
-                  <Form.Label>Last Name</Form.Label>
-                  <Form.Control type="text" placeholder="Last Name" />
-                </Form.Group>
-              </Row>
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridEmail">
-                  <Form.Label>Email</Form.Label>
-                  <Form.Control
-                    type="email"
-                    placeholder="Enter email"
-                    required
-                  />
-                </Form.Group>
+						<Form.Group as={Col} controlId="formGridLastName">
+							<Form.Label>Last Name:</Form.Label>
+							<Form.Control type="text" placeholder="Last Name" />
+						</Form.Group>
+					</Row>
+					<Row className="mb-3">
+						<Form.Group as={Col} controlId="formGridEmail">
+							<Form.Label>Email:</Form.Label>
+							<Form.Control type="email" placeholder="Enter email" required />
+						</Form.Group>
 
-                <Form.Group as={Col} controlId="formGridPassword">
-                  <Form.Label>Password</Form.Label>
-                  <Form.Control
-                    type="password"
-                    placeholder="Password"
-                    required
-                  />
-                </Form.Group>
-              </Row>
+						<Form.Group as={Col} controlId="formGridPassword">
+							<Form.Label>Password:</Form.Label>
+							<Form.Control type="password" placeholder="Password" required />
+						</Form.Group>
+					</Row>
 
-              <Form.Group className="mb-3" controlId="formGridAddress1">
-                <Form.Label>Address</Form.Label>
-                <Form.Control placeholder="1234 Main St"  />
-              </Form.Group>
+					<Form.Group className="mb-3" controlId="formGridAddress1">
+						<Form.Label>Address:</Form.Label>
+						<Form.Control placeholder="1234 Main St" />
+					</Form.Group>
 
-              <Form.Group className="mb-3" controlId="formGridTelephone">
-                <Form.Label>Telephone Number</Form.Label>
-                <Form.Control placeholder="Telephone Number"  />
-              </Form.Group>
+					<Form.Group className="mb-3" controlId="formGridTelephone">
+						<Form.Label>Telephone Number:</Form.Label>
+						<Form.Control placeholder="Telephone Number" />
+					</Form.Group>
 
-              <Row className="mb-3">
-                <Form.Group as={Col} controlId="formGridCity" >
-                  <Form.Label>City</Form.Label>
-                  <Form.Control />
-                </Form.Group>
+					<Row className="mb-3">
+						<Form.Group as={Col} controlId="formGridCity">
+							<Form.Label>City:</Form.Label>
+							<Form.Control />
+						</Form.Group>
 
-                <Form.Group as={Col} controlId="formGridCountry" >
-                  <Form.Label>Country</Form.Label>
-                  <Form.Select defaultValue="Choose...">
-                    {Country.map(option => (
-                      <option key={option} value={option}>
-                        {option}
-                      </option>
-                    ))}
-                  </Form.Select>
-                </Form.Group>
-                <Form.Group as={Col} controlId="formGridZip" >
-                  <Form.Label>Zip</Form.Label>
-                  <Form.Control />
-                </Form.Group>
-              </Row>
+						<Form.Group as={Col} controlId="formGridCountry" className='countryBox'>
+							<Form.Label >Country:</Form.Label> 
+							<Form.Select defaultValue="Choose..."class="form-control input-lg">
+								{Country.map((option) => (
+									<option key={option} value={option}>
+										{option}
+									</option>
+								))}
+							</Form.Select>
+						</Form.Group>
+						<Form.Group as={Col} controlId="formGridZip">
+							<Form.Label>Zip:</Form.Label>
+							<Form.Control />
+						</Form.Group>
+					</Row>
 
-              <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check
-                  type="checkbox"
-                  label="By checking this box you have agreed to all our Terms And Conditions"
-                  required
-                />
-              </Form.Group>
-              <Form.Group className="mb-3" id="formGridCheckbox">
-                <Form.Check
-                  type="checkbox"
-                  label="By checking this box you have agreed to send you emails with latest additions to our products"
-                />
-              </Form.Group>
+					<Form.Group className="mb-3" id="formGridCheckbox">
+						<Form.Check
+							type="checkbox"
+							label="By checking this box you have agreed to all our of Terms And Conditions."
+							required
+						/>
+					</Form.Group>
+					<Form.Group className="mb-3" id="formGridCheckbox">
+						<Form.Check
+							type="checkbox"
+							label="By checking this box you have agreed to have us send you emails with all of the latest additions to our products."
+						/>
+					</Form.Group>
 
-              <Button variant="primary" type="submit">
-                Submit
-              </Button>
-            </Form>
-          </Container>
-        </Popover.Body>
-      </Popover>
-    </div>
-  );
+					<Button variant="primary" type="submit">
+						Submit
+					</Button>
+				</Form>
+			</Container>
+		</div>
+	);
 }
 
 export default Signup;
